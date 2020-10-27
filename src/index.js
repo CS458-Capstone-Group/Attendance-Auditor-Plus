59ab189be680dc34db1e2d7fcf562414435eeeb9
Skye Gibney <skyejgibney@gmail.com>
2020-10-26 22:42:58 -0700

    added routes to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 //This is the server
 const express = require("express");
 
+const eventsRouter = require("./routes/api/events.js");
+const guestsRouter = require("./routes/api/guests.js");
+const inventoryRouter = require("./routes/api/inventory.js");
+const organizersRouter = require("./routes/api/organizers.js");
+const usersRouter = require("./routes/api/users.js");
+
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+app.use("/api/events", eventsRouter);
+app.use("/api/guests", guestsRouter);
+app.use("/api/inventory", inventoryRouter);
+app.use("/api/organizers", organizersRouter);
+app.use("/api/users", usersRouter);
 
 app.get("/", (request, response) =>
 {
