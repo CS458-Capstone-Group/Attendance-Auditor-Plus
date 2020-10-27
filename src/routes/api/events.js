57750fd3fcc02ef4410c1dbc64a66297e1837921
Skye Gibney <skyejgibney@gmail.com>
2020-10-26 21:58:17 -0700

    add /api/event/* routes

---
+++ src/routes/api/events.js
@@ -0,0 +1,54 @@
+/*
+    This module handles routes for /api/events/
+*/
+
+const express = require("express");
+
+const router = express.Router();
+
+// Get a subset of events
+router.get("/", (req, res) => {
+
+});
+
+// Create an event
+router.post("/", (req, res) => {
+
+});
+
+// Get a specific event
+router.get("/:eventId", (req, res) => {
+
+});
+
+// Edit a specific event
+router.post("/:eventId", (req, res) => {
+
+});
+
+// Delete a specific event
+router.delete("/:eventId", (req, res) => {
+
+});
+
+// Create a member's attendance entry
+router.post("/:eventId/attendance/member", (req, res) => {
+
+});
+
+// Create a guest's attendance entry
+router.post("/:eventId/attendance/guests", (req, res) => {
+
+});
+
+// Edit a member's attendance entry
+router.post("/:eventId/attendance/member/:memberId", (req, res) => {
+
+});
+
+// Edit a guest's attendance entry
+router.post("/:eventId/attendance/guest/:guestId", (req, res) => {
+
+});
+
+module.exports = router;
